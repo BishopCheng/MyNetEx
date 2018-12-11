9143dfcfae1728db956b11a5ad058634d24b85e0
intothesun <intothesun@163.com>
2018-12-11 08:37:19 +0800

    add OverLoadClass

---
+++ MyEx/OverLoad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEx
+{
+    /// <summary>
+    /// 重载练习类
+    /// </summary>
+    /// 
+
+
+    public class OverLoad
+    {
+        public void GetMember(string nameInput)
+        {
+           
+        }
+    }
+}
